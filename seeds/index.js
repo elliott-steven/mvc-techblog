3ccd975a82710338c48a2a91fa900f6bbaf3c238
Steven Elliott <steven.elliott@live.com>
2021-04-01 21:58:02 -0400

    adding blank route files

---
+++ seeds/index.js
@@ -0,0 +1,25 @@
+
+const seedPosts = require('./seed-posts');
+const seedUsers = require('./seed-users');
+const seedComments = require('./seed-comments');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+
+  await sequelize.sync({ force: true });
+    console.log('\n----- DB IS SYNCED -----\n');
+  
+  await seedUsers();
+    console.log('\n----- SEEDED THE USERS -----\n');
+  
+  await seedPosts();
+    console.log('\n----- SEEDED THE POSTS -----\n');
+
+  await seedComments();
+    console.log('\n----- SEEDED THE COMMENTS -----\n');
+
+  process.exit(0);
+};
+
+seedAll();
